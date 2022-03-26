95a8c4d5f3ca42c60267ab9c946a391fc33c83db
Arvin-2005 <102425221+Arvin-2005@users.noreply.github.com>
2022-03-26 20:50:16 +0430

    Add files via upload

---
+++ max,min/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace max_min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int max = a;
+            int min = a;
+
+            if (b > max) max = b;
+            if (c > max) max = c;
+
+            if (b < min) min = b;
+            if (c < min) min = c;
+
+            Console.Write("max = {0} min = {1}", max, min);
+
+        }
+    }
+}
